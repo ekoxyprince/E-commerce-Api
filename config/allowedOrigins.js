9a919dd6c623cdc7c5f65c09ea3c1be3cf59adb3
Echefula Ndukwe <111073744+Eche5@users.noreply.github.com>
2024-08-02 22:49:05 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
+  "https://client-pi-sooty.vercel.app",
   "http://127.0.0.1:5500",
+  "http://localhost:5173",
   "http://localhost:3000",
 ];
 
