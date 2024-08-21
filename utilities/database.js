a509a3d5cc78ec2504179f59d98c9471c85bf857
Echefula Ndukwe <111073744+Eche5@users.noreply.github.com>
2024-08-21 12:38:28 +0100

    test

--- utilities/database.js
+++ utilities/database.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose')
-const {database_uri} = require('../config')
+const mongoose = require("mongoose");
+const { database_uri } = require("../config");
 
-module.exports = ()=>{
-   return mongoose.connect(database_uri,{
-    dbName:'urbantrovedb',
-    autoIndex:false,
-   //  tls:true,
-   })
-}
+module.exports = () => {
+  return mongoose.connect(database_uri, {
+    dbName: "urbantrovedb",
+    autoIndex: false,
+    //  tls:true,
+  });
+};
