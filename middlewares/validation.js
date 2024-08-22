abd6043eec887b6deb9ab60fb7be27996d2aee6c
Echefula Ndukwe <111073744+Eche5@users.noreply.github.com>
2024-08-22 18:15:13 +0100

    password validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -12,9 +12,7 @@ exports.password = body("password")
   .withMessage("The password field requires a value")
   .isLength({ min: 8 })
   .withMessage("password must be at least 8 characters")
-  .matches(
-    /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-  )
+  .matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W)[A-Za-z\d\W]{8,}$/)
   .withMessage(
     "Password must contain atleast one lowercase, one uppercase, one digit and one special character"
   )
