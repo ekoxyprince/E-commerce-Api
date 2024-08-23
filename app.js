9d9b2b02c42cb1f0334e61dacbcd892055049ac5
Echefula Ndukwe <111073744+Eche5@users.noreply.github.com>
2024-08-23 22:57:31 +0100

    b

--- app.js
+++ app.js
@@ -38,12 +38,8 @@ app.use(compression());
 app.use(helmet());
 app.use(logger("dev"));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: allowedOrigin,
-    credentials: true,
-  })
-);
+app.use(cors({ origin: true, credentials: true }));
+
 
 app.use(
   session({
