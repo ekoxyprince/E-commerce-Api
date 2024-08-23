50db69af41f7eb4b9b8a0570cacd299c76db8dde
Echefula Ndukwe <111073744+Eche5@users.noreply.github.com>
2024-08-23 23:44:59 +0100

    test

--- app.js
+++ app.js
@@ -30,8 +30,6 @@ const store = new MongoDBStore({
   uri: database_uri,
   collection: "sessions",
 });
-app.set("trust proxy", true);
-
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
