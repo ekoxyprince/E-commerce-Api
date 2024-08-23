6eb1a0ee61c68d67a12171249d3d632e4fa783a0
Echefula Ndukwe <111073744+Eche5@users.noreply.github.com>
2024-08-23 19:52:42 +0100

    test

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
-  "https://urbantrov.com.ng",
-  "https://client-pi-sooty.vercel.app",
+  "http://localhost:5173",
   "http://127.0.0.1:5500",
   "http://localhost:5173",
   "http://localhost:3000",
